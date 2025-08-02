c5d2ca47eaa0a9782dc51b4da1fc1cfffbb2f927
Renan Arantes <malevo18@gmail.com>
2025-08-02 19:21:14 -0300

    lesson: filling the board

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -5,7 +5,12 @@ import Board from "./components/Board";
 import { BoardState } from "./types";
 
 function App() {
-  const [board, setBoard] = useState<BoardState[]>(Array(9).fill(null));
+  const [board, setBoard] = useState<BoardState>(Array(9).fill(null));
+
+  const handleClick = (index: number) => {
+    console.log("clicked: " + index);
+    setBoard(board.map((value, i) => (i === index ? "X" : value)));
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-blue-200">
@@ -17,7 +22,7 @@ function App() {
         <div className="mb-6 text-center">
           <p className="text-xl font-semibold text-gray-100">Player X's turn</p>
         </div>
-        <Board board={board} />
+        <Board board={board} onClick={handleClick} />
       </div>
     </main>
   );
--- tic-tac-toe/src/components/Board.tsx
+++ tic-tac-toe/src/components/Board.tsx
@@ -3,13 +3,14 @@ import Square from "./Square";
 
 type BoardProps = {
   board: BoardState;
+  onClick: (index: number) => void;
 };
 
-function Board({ board }: BoardProps) {
+function Board({ board, onClick }: BoardProps) {
   return (
     <div className="mx-auto grid max-w-[26rem] grid-cols-3 gap-4">
       {board.map((value, index) => (
-        <Square key={index} value={value} />
+        <Square key={index} value={value} onClick={() => onClick(index)} />
       ))}
     </div>
   );
